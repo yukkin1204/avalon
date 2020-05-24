36e97ffe625be38a9c3c11900c1cab172f5241be
yukkin1204 <yukkin1204.mathematics@gmail.com>
2020-05-24 18:10:01 +0900

    initial commit

---
+++ main/views.py
@@ -0,0 +1,9 @@
+import flask 
+from main import app
+from main.models import Entry
+
+@app.route('/')
+def show_entries():
+    entries = Entry.query.all()
+    return flask.render_template('entries.html', entries=entries)
+
