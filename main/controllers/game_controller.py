fbf5b906afcdb701249fd41d6ce157816149d108
yukkin1204 <yukkin1204.mathematics@gmail.com>
2020-06-05 23:18:03 +0900

    ファイル名の軽微な変更

--- main/controllers/game_controller.py
+++ main/controllers/game_controller.py
@@ -6,5 +6,5 @@ from main.models.game import Game
 @app.route('/games', methods=['GET'])
 def show_game_list():
     games = Game.list()
-    return render_template('games/game_index.html', title='Game List', games=games)
+    return render_template('games/index.html', title='Game List', games=games)
 
